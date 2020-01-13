0b91f0d1b7858dd2c4a444a5c7d88c754ba78013
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2020-01-13 17:23:58 +0900

    Check if constructor is called

--- tests/exe.c
+++ tests/exe.c
@@ -4,14 +4,26 @@
 #include "base.h"
 #include "lib.h"
 
+static int g_init;
+__attribute__((constructor)) static void init() {
+    puts("Constructor");
+    g_init = 42;
+}
+
+__attribute__((destructor)) static void quit() {
+    puts("Destructor");
+}
+
 int main() {
-    puts("Start");
+    puts("Start main");
     if (lib_add_42_via_base(10) != 52) abort();
     if (in_both_lib_and_base() != 100) abort();
 
     fprintf(stderr, "Use stderr from main\n");
     lib_use_stderr();
     base_use_stderr();
+
+    if (g_init != 42) abort();
 
     puts("OK");
 }
