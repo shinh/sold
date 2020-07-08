d4c8f61f2df1534d1fa5d6a9d01a15bb89f08c5b
Akira Kawata <akirakawata1@gmail.com>
2020-07-08 09:48:00 +0900

    Write a description of print_dynsymtab

--- print_dynsymtab.cc
+++ print_dynsymtab.cc
@@ -1,3 +1,13 @@
+//
+// print_dynsymtab
+//
+// This program parses and prints DT_SYMTAB of a given ELF file. It prints all symbols
+// and version information. When the ELF file does not include version information,
+// it prints NO_VERSION_INFO instead of version information.
+//
+// Unlike nm -D --with-symbol-versions or readelf --dynsyms, it works without any shdrs
+// because print_dynsymtab gets all information only from phdrs.
+
 #include "elf_binary.h"
 
 #include <iostream>
