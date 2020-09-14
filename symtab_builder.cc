8d762e4c7bf1f6af695a9930cadc37a5bb52a3d1
Akira Kawata <akirakawata1@gmail.com>
2020-09-14 00:59:21 +0000

    Update if not found or undefined

--- symtab_builder.cc
+++ symtab_builder.cc
@@ -49,7 +49,7 @@ bool SymtabBuilder::Resolve(const std::string& name, const std::string& soname, 
         Syminfo* found = NULL;
         for (int i = 0; i < src_syms_.size(); i++) {
             if (src_syms_[i].name == name && src_syms_[i].soname == soname && src_syms_[i].version == version) {
-                found = &src_syms_[i];
+                if (found == NULL || IsDefined(*src_syms_[i].sym)) found = &src_syms_[i];
             }
         }
 
