aad42c00a8bfb31cd2a8575b99b0644aed05a55e
Akira Kawata <akirakawata1@gmail.com>
2020-11-23 13:27:27 +0000

    Test access to global variable throught dlopen

--- tests/tls-dlopen-gcc/lib.c
+++ tests/tls-dlopen-gcc/lib.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-__thread int tls_data = 0;
-__thread int tls_bss;
+__thread int tls_with_init_value = 0;
+__thread int tls_without_init_value;
 
 void show_tls_variables() {
-    tls_data = 10;
-    tls_bss = 10;
-    printf("tls_data = %d, tls_bss = %d\n", tls_data, tls_bss);
+    tls_with_init_value = 10;
+    tls_without_init_value = 10;
+    printf("tls_with_init_value = %d, tls_without_init_value = %d\n", tls_with_init_value, tls_without_init_value);
 }
--- tests/tls-dlopen-gcc/main.c
+++ tests/tls-dlopen-gcc/main.c
@@ -6,6 +6,12 @@ int main() {
     if (handle != NULL) {
         void (*func)() = (void (*)())dlsym(handle, "show_tls_variables");
         func();
+
+        int* tls_with_init_value_p = (int*)dlsym(handle, "tls_with_init_value");
+        int* tls_without_init_value_p = (int*)dlsym(handle, "tls_without_init_value");
+
+        printf("*tls_with_init_value_p = %d, *tls_without_init_value_p = %d\n", *tls_with_init_value_p, *tls_without_init_value_p);
+
     } else {
         printf("Cannot open lib.so\n");
     }
