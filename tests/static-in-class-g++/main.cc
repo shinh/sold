fe1102e624be71c8ca1e158f230858fc727dbde5
Akira Kawata <akirakawata1@gmail.com>
2020-12-07 08:11:54 +0000

    Add tests to github workflow

---
+++ tests/static-in-class-g++/main.cc
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << func() << std::endl;
+}
--- tests/tls-bss-g++/test.sh
+++ tests/tls-bss-g++/test.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -eu
 
-g++ -Wl,--hash-style=gnu -o main main.cc
-
+g++ -o main main.cc
 ../../build/sold main -o main.out --section-headers
 ./main.out
