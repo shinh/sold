04f9e00aa10e21bca0c78fc36764181d241c6ec3
Akira Kawata <akirakawata1@gmail.com>
2021-02-01 15:04:31 +0900

    Set LICENSE

--- mprotect_builder.h
+++ mprotect_builder.h
@@ -1,3 +1,18 @@
+// Copyright (C) 2021 The sold authors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #pragma once
 
 #include <stdint.h>
@@ -36,6 +51,8 @@ public:
         free(mprotect_code);
     }
 
+    // We apply COPYING.RUNTIME for memprotect_body_code and memprotect_end_code.
+    //
     // call SYS_mprotect syscall
     //
     // mov $0xdeadbeefdeadbeef, %rdi
