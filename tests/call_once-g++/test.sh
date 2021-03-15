c98257551c31aa79c83d0581ed6bedea898bc387
Akira Kawata <akirakawata1@gmail.com>
2021-03-15 01:22:18 +0000

    Fix a test

--- tests/call_once-g++/test.sh
+++ tests/call_once-g++/test.sh
@@ -2,7 +2,7 @@
 
 g++ -fPIC -shared -Wl,-soname,libhoge.so -o libhoge.so hoge.cc
 g++ -fPIC -shared -Wl,-soname,libfuga.so -o libfuga.so fuga.cc libhoge.so
-g++ -o main main.cc libfuga.so -pthread
+LD_LIBRARY_PATH=. g++ -o main main.cc libfuga.so -pthread
 
 mv libfuga.so libfuga.so.original
 GLOG_log_dir=. LD_LIBRARY_PATH=. ../../build/sold -i libfuga.so.original -o libfuga.so.soldout --section-headers
