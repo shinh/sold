1b715c5bfd9072c2cb13c93adfaa344e0be1080d
Akira Kawata <akirakawata1@gmail.com>
2021-09-15 21:50:09 +0900

    Remove GLOG_logtostderr=1 for debug

--- tests/renamer/test.sh
+++ tests/renamer/test.sh
@@ -4,6 +4,6 @@ gcc -fPIC -fpic -shared -o libhoge_original.so hoge.c -Wl,-soname,libhoge.so
 gcc -fPIC -fpic -shared -o libfugafuga.so fugafuga.c -Wl,-soname,libhoge.so
 ln -sf libfugafuga.so libhoge.so
 gcc -o use_fugafuga use_fugafuga.c libhoge.so
-GLOG_logtostderr=1 ../../build/renamer libhoge_original.so --output libhoge_renamed.so --rename-mapping-file mapping
+../../build/renamer libhoge_original.so --output libhoge_renamed.so --rename-mapping-file mapping
 ln -sf libhoge_renamed.so libhoge.so
 LD_LIBRARY_PATH=. ./use_fugafuga
